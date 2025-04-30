dea8599988548b67e1ca72433a760fcd61fb980b
IvoryMay <theintsandikyaw1@gmail.com>
2025-04-30 23:05:33 +0630

    Theme removed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,6 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  theme: {
-    extend: {
-      colors: {
-        darkTheme: "#11001F",
-      },
-      fontFamily: {
-        Outfit: ["Outfit", "sans-serif"],
-        Ovo: ["Ovo", "serif"],
-      },
-      boxShadow:{
-      "black" :"4px 4px 0 #000",
-      "white" :"4px 4px 0 #fff"
-      },
-      
-    },
-  },
+  
   darkMode : 'class',
 };
 
